c656062982cba893c9a9f392b36db1347afc9162
Gokul Gopinath <gokul4e@gmail.com>
2026-07-11 00:25:47 +0530

    responsive login page

--- login.php
+++ login.php
@@ -6,14 +6,16 @@ include("db.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Login</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-light">
 
-<div class="container mt-5">
-    <div class="card shadow p-4 mx-auto" style="max-width: 400px;">
+<div class="container mt-5 px-3">
+    <div class="card shadow p-4 mx-auto w-100" style="max-width: 450px;">
         <h2 class="text-center text-primary">
             Admin Login
         </h2>
